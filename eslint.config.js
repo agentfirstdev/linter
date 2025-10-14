7d12b04f856869487b028a9f856fd60ec92c06f0
Brian Kennish <oldestlivingboy@gmail.com>
2025-10-14 01:04:52 -0500

    Ditched Babel.

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
+import filesystem from 'node:fs';
 import { defineConfig } from 'eslint/config';
 import eslint from '@eslint/js';
 import jsonc from 'eslint-plugin-jsonc';
 import react from 'eslint-plugin-react';
 import prettier from 'eslint-plugin-prettier';
 import jsoncParser from 'jsonc-eslint-parser';
-import babelParser from '@babel/eslint-parser';
 import globals from 'globals';
+
+const prettierConfig = JSON.parse(
+  filesystem.readFileSync(new URL('.prettierrc', import.meta.url), 'utf8')
+);
 
 export default defineConfig([
   {
@@ -13,13 +17,7 @@ export default defineConfig([
     extends: [eslint.configs.recommended],
     languageOptions: {
       globals: { ...globals.node, ...globals.browser },
-      parser: babelParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        requireConfigFile: false,
-        babelOptions: { presets: ['@babel/preset-react'] }
-      }
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: { jsx: true } }
     },
     plugins: { react, prettier },
     rules: {
@@ -29,14 +27,15 @@ export default defineConfig([
       'react/jsx-uses-react': 'warn',
       'react/jsx-uses-vars': 'warn',
       'react/react-in-jsx-scope': 'off',
-      'prettier/prettier': 'warn'
+      'prettier/prettier': ['warn', prettierConfig]
     },
     settings: { react: { version: 'detect' } }
   },
+  { files: ['**/*.cjs'], languageOptions: { parserOptions: { sourceType: 'commonjs' } } },
   {
     files: ['**/*.json'],
     languageOptions: { parser: jsoncParser },
     plugins: { jsonc, prettier },
-    rules: { 'prettier/prettier': 'warn' }
+    rules: { 'prettier/prettier': ['warn', prettierConfig] }
   }
 ]);
